e18602db9ed17585b2e1f1d36eac376afae3f3f7
namhanminh2 <gflsofm1309@gmail.com>
2024-04-03 03:12:26 +0700

    nah

--- Assets/Script/Script_Enemy/Enemy_Trunk.cs
+++ Assets/Script/Script_Enemy/Enemy_Trunk.cs
@@ -8,6 +8,9 @@ public class Enemy_Trunk : Enemy
     [Header("Trunk specific")]
     [SerializeField] private float checkRadius;
     [SerializeField] private LayerMask whatIsPlayer;
+    [SerializeField] private Transform groundBehindCheck;
+    private bool wallBehind;
+    private bool groundBehind;
 
 
 
@@ -16,6 +19,9 @@ public class Enemy_Trunk : Enemy
     [Header("Bullet specific")]
     [SerializeField] private float attackCooldown;
     private float attackCooldownCounter;
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform bulletOrigin;
 
 
     protected override void Start()
@@ -26,33 +32,76 @@ public class Enemy_Trunk : Enemy
     void Update()
     {
         CollisionCheck();
-        
+
+        if (!canMove)
+            rb.velocity = new Vector2(0, 0);
 
         attackCooldownCounter -= Time.deltaTime;
-        if(playerDetection.collider.GetComponent<Player>() != null)
+        if (playerDetection.collider.GetComponent<Player>() != null)
         {
-            if(attackCooldownCounter < 0 )
+            if (attackCooldownCounter < 0)
             {
                 attackCooldownCounter = attackCooldown;
                 anim.SetTrigger("attack");
                 canMove = false;
             }
+            else if (playerDetection.distance < 3)
+                MoveBackwards(1.5f);
         }
         else
         {
-        WalkAround();
+            if (playerDetected)
+                MoveBackwards(4);
+            else
+                WalkAround();
 
         }
+
+        anim.SetFloat("xVelocity", rb.velocity.x);
     }
+
+    private void MoveBackwards(float multiplier)
+    {
+        if (wallBehind)
+            return;
+        if (!groundBehind)
+            return;
+
+        rb.velocity = new Vector2(speed * multiplier * -facingDirection, rb.velocity.y);
+    }
+
+
+
+    private void AttackEvent()
+    {
+        GameObject newBullet = Instantiate(bulletPrefab, bulletOrigin.transform.position, bulletOrigin.transform.rotation);
+
+        newBullet.GetComponent<Bullet>().SetupSpeed(bulletSpeed * facingDirection, 0);
+    }
+
+    private void ReturnMovement()
+    {
+        canMove = true;
+    }
+
 
     protected override void CollisionCheck()
     {
         base.CollisionCheck();
 
         playerDetected = Physics2D.OverlapCircle(transform.position, checkRadius, whatIsPlayer);
-
+        groundBehind = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        wallBehind = Physics2D.Raycast(wallCheck.position, Vector2.right * (-facingDirection + 1), wallCheckDistance, whatIsGround);
     }
 
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
 
+        Gizmos.DrawWireSphere(transform.position, checkRadius);
+
+        Gizmos.DrawLine(groundBehindCheck.position, new Vector2(groundBehindCheck.position.x, groundBehindCheck.position.y - groundCheckDistance));
+
+    }
 
 }
